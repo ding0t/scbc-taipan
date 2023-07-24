78947a3dc46a4ffe1a62aa3f853099fa314f5fbb
ding0t <126217954+ding0t@users.noreply.github.com>
2023-07-24 14:01:19 +0800

    added playground

---
+++ playground/init_play.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+#
+# ABOUT
+# Setup and Ubuntu or Debian host with a mock challenge
+#
+# Author: ding0t
+#
+# REFERENCES
+# https://google.github.io/styleguide/shellguide.html
+#####################
+
+# Check if running as root
+if [[ $EUID -ne 0 ]]
+then
+  printf "You must use sudo to run this script as root. Exiting."
+  exit 1
+fi
+
+source $(dirname "$0")/lib/users.sh
+users_csv_file= '$(dirname "$0")/config/users.csv'
+
+printf ${users_csv_file}
+
