ded8e2f43ae6ddde7230c204cf9f637f46091fa9
ding0t <126217954+ding0t@users.noreply.github.com>
2023-07-28 19:45:32 +0800

    Update menu.sh

--- fightclub/scripts/menu.sh
+++ fightclub/scripts/menu.sh
@@ -79,7 +79,7 @@ function print_menu(){
     #  Show options like
     # 1) "Option"
     for i in in "${!A_OPTIONS[@]}"; do
-        printf "${i}) A_OPTIONS[${i}]"
+        printf "${i}) ${A_OPTIONS[${i}]}\n"
     done
 }
 
